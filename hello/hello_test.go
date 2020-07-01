877eefa2642857c261555a3c6d4d6ecc6f0f1784
Samuel Stenton <sthstenton@gmail.com>
2020-07-01 21:29:30 +0100

    further hello world refactors and move to hello directory

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,15 +12,29 @@ func TestHello(t *testing.T) {
 		}
 	
 	t.Run("saying hello to someone", func(t *testing.T) {
-		got := Hello("Sam")
+		got := Hello("Sam", "")
 		want := "Hello, Sam"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("...in spanish", func(t *testing.T) {
+		got := Hello("Sam", "Spanish")
+		want := "Hola, Sam"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("...in French", func(t *testing.T) {
+		got := Hello("Sam", "French")
+		want := "Bonjour, Sam"
 
 		assertCorrectMessage(t, got, want)
 	})
