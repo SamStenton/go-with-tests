9f7b2eae9c938e54bc6722475ec32fbadf3a7ff3
Samuel Stenton <sthstenton@gmail.com>
2020-07-01 21:33:55 +0100

    formatting

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t *testing.T, got, want string) {
-        t.Helper()
+		t.Helper()
 		if got != want {
-				t.Errorf("got %q want %q", got, want)
-			}
+			t.Errorf("got %q want %q", got, want)
 		}
+	}
 	
 	t.Run("saying hello to someone", func(t *testing.T) {
 		got := Hello("Sam", "")
